015724777e775b77234803115c6179b5dedc9abb
PranitMandaokar <109650592+PranitMandaokar@users.noreply.github.com>
2022-07-20 13:42:43 +0530

    Add files via upload

---
+++ A1Q3.py
@@ -0,0 +1,18 @@
+# Write a Python program to count the number of even and odd numbers from a series of numbers.
+
+
+series = [5,10,15,20,25,30,35,40,45,50]  # given series
+
+print("numbers : ",series)          # to print the series
+
+e = 0
+o = 0
+
+for i in series:                          # logic to get even and odd number 
+    if i % 2 == 0:
+        e += 1
+    else:
+        o += 1
+
+print("Number of Even numbers :",e)    #to print the number of even and odd number
+print("Number of Odd numbers :",o)
